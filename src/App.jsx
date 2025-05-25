0e027b4940130a6d163ac14ea92e8a755522aef9
Huzaif-Ibrahim <i.huzaif.ibrahim@gmail.com>
2025-05-25 11:27:09 +0530

    Change4

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <div id="howitworks"><HowItWorks/></div>
       <div id="features"><Features/></div>
       <div id="pricing"><Plans/></div>
-      <Testimonials/>
+      <div id="testimonials"><Testimonials/></div>
       <Footer/>
       
     </main>
